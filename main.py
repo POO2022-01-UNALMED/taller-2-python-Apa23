21311de3d0a2274ebfc15ff7c0ad7c50f95320ec
Andrés Aparicio <andres.apa.23@gmail.com>
2022-03-20 14:16:06 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+class auto:
+    cantidadCreados=0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, ):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+        return len(self.asientos)
+    
+    def verificarIntegridad(self):
+        regMotor= self.motor.registro
+        if(regMotor!=self.registro):
+            return "Las piezas no son originales"
+        regAsientos=self.asientos[0]
+        for i in self.asientos:
+            if(regAsientos!=i.registro):
+                return "Las piezas no son originales"
+            regAsientos =i.registro
+        if(regAsientos!=self.registro):
+            return "Las piezas no son originales"
+        return "Auto original"
+
+        
+class motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro=registro
+    
+    def asignarTipo(self, tipo):
+        if(tipo == "electrico" or tipo=="gasolina"):
+            self.tipo = tipo
+        
+
+
+class asiento:
+    def __init__(self, color, precio, registro):
+        self.color= color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        colores=["rojo","amarillo","negro","blanco","verde","amarillo"]
+        if(colores.contains(color)):
+            self.color = color
+
